3f51fa830f1b9607d41648620b4b28fe704fb54c
Lukas Brandenberger <brandluk@students.zhaw.ch>
2019-01-15 13:50:04 +0100

    Kapitel 8.2 beendet

--- src/main/java/ch/zhaw/gpi/twitterreview/delegates/SendTweetDelegate.java
+++ src/main/java/ch/zhaw/gpi/twitterreview/delegates/SendTweetDelegate.java
@@ -1,9 +1,11 @@
 
 package ch.zhaw.gpi.twitterreview.delegates;
 
+import ch.zhaw.gpi.twitterreview.services.TwitterService;
 import javax.inject.Named;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Implementation des Service-Task Tweet-Senden
@@ -11,16 +13,25 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
  */
 @Named("sendTweetAdapter")
 public class SendTweetDelegate implements JavaDelegate {
+    
+  @Autowired
+    private TwitterService twitterService;
 
     /**
-     * 
-     * @param de            Objekt , welches Verknüasüdigh
-     * @throws Exception
-     */
-    @Override
-    public void execute(DelegateExecution de) throws Exception {
-        String tweetContent = (String) de.getVariable("tweetContent");
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!! Folgender Tweet wird veröffentlicht: " + tweetContent);
-    }
+ * Postet einen Tweet mit dem gewünschten Text
+ *
+ * @param de Objekt, welches die Verknüpfung zur Process Engine und zur aktuellen Execution enthält
+ * @throws Exception
+ */
+ @Override
+ public void execute(DelegateExecution de) throws Exception {
+ // Prozessvariable tweetContent wird ausgelesen
+ String tweetContent = (String) de.getVariable("tweetContent");
+
+ // Dieser Text wird dem Twitter Service an die Methode updateStatus übergeben
+ twitterService.updateStatus(tweetContent);
+ }
+    
     
 }
+ 
